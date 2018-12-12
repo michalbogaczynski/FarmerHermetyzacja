74334b7da655e818899043ccb9dbbe3144359f72
michalbogaczynski <bogaczynski.m@gmail.com>
2018-12-12 23:14:33 +0100

    Na podstawie programu zobrazowano hermetyzacje (get set, inicjalizator, konstruktor) 255 str

---
+++ FarmerGetSet/Farmer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmerGetSet
+{
+    public class Farmer
+    {
+        public int BagsOfFeed { get; private set; }
+        private int feedMultiplier;
+        public int FeedMultiplier { get { return feedMultiplier; } }
+
+        private int numberOfCows;
+        public int NumberOfCows
+        {
+            get
+            {
+                return numberOfCows;
+            }
+            set
+            {
+                numberOfCows = value;
+                BagsOfFeed = numberOfCows * FeedMultiplier;
+            }
+
+            
+
+        }
+
+        public Farmer(int numberOfCows, int feedMultiplier)
+        {
+            this.feedMultiplier = feedMultiplier;
+            NumberOfCows = numberOfCows;
+        }
+
+    }
+}
--- FarmerGetSet/Form1.cs
+++ FarmerGetSet/Form1.cs
@@ -12,9 +12,27 @@ namespace FarmerGetSet
 {
     public partial class Form1 : Form
     {
+        Farmer farmer;
+
         public Form1()
         {
             InitializeComponent();
+            farmer = new Farmer(15, 30); // { NumberOfCows = 15, };  inicjalizator obiketu w przypadku gdy pola sa prywatne wyrzuci blad, należy użyć kontruktora. Pole feedMultiplier = 30 jest prywatne | inicjalizator został zastąpiony kontruktorem
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            farmer.NumberOfCows = (int)numericUpDown1.Value;
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Potrzebuję {0} worków paszy do wykarmienia {1} krów", farmer.BagsOfFeed, farmer.NumberOfCows);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //farmer.BagsOfFeed = 5;
         }
     }
 }
